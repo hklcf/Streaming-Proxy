d6d1dadeed74b0241743fb17eba73f388a5e376b
HKLCF <hklcfs@gmail.com>
2018-06-30 12:47:43 +0800

    add hls_list_size config

--- stream.sh
+++ stream.sh
@@ -29,5 +29,5 @@ do
     rm -rf $WEB_PATH/*
   fi
 
-  $FFMPEG -y -i "$SOURCE" -c copy -copyts -hls_time 10 -hls_flags delete_segments+second_level_segment_index+second_level_segment_size+second_level_segment_duration -use_localtime 1 -hls_segment_filename "$WEB_PATH/segment_%Y%m%d%H%M%S_%%04d_%%08s_%%013t.ts" "$WEB_PATH/playlist.m3u8"
+  $FFMPEG -y -i "$SOURCE" -c copy -copyts -hls_time 10 -hls_list_size 5 -hls_flags delete_segments+second_level_segment_index+second_level_segment_size+second_level_segment_duration -use_localtime 1 -hls_segment_filename "$WEB_PATH/segment_%Y%m%d%H%M%S_%%04d_%%08s_%%013t.ts" "$WEB_PATH/playlist.m3u8"
 done
